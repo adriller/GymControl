96b784ed3ca08d87ce939fd99120833f12f706b1
Adriller Ferreira <adriller_gf@hotmail.com>
2017-09-12 17:53:46 +0200

    funcoes do cliente implementadas

--- www/js/functions.js
+++ www/js/functions.js
@@ -1,31 +1,16 @@
 function select(id, nameWk){
   $('#' + id).addClass("w3-black");
 
-  var prevWork = localStorage.selectedWorkout;
+  var prevWork = localStorage.selectedWkID;
 
   if(prevWork != null && prevWork != id)
     $('#' + prevWork).removeClass("w3-black");
 
   $("#btnStart").removeClass("w3-disabled");
-  $('#btnStart').removeAttr("disabled")
+  $('#btnStart').removeAttr("disabled");
 
-  localStorage.selectedWorkout = id;
+  localStorage.selectedWkID = id;
   localStorage.selectedWkName =  nameWk;
-}
-
-function editEx(id){
-  document.getElementById('modalEdit').style.display='block'
-  //put in fields the data
-  //send changes to db
-}
-
-function addEx(){
-  document.getElementById('modalEdit').style.display='block';
-  //send to db
-}
-
-function doneEx(){
-  //next ex
 }
 
 /*Controls quantity*/
@@ -69,13 +54,15 @@ function getWorkouts( ){
 }
 
 function getExercices(){
-  //get name and ID
+  var wkID = localStorage.selectedWkID;
+
+  //request eercices of workout wkID
   var exObj = {
     "exercices" : [
-      { "ID" : 1, "name" : "Bench Press", "weight" : "50", "reps" : 4, "rest" : 90},
-      { "ID" : 2, "name" : "Bench Press Declined", "weight" : "45", "reps" : 4, "rest" : 90 },
-      { "ID" : 3, "name" : "Contractor", "weight" : "27", "reps" : 3, "rest" : 90 },
-      { "ID" : 4, "name" : "Converging", "weight" : "45", "reps" : 4, "rest" : 90 }
+      { "ID" : 1, "name" : "Bench Press", "weight" : "50", "reps" : 2, "rest" : 3},
+      { "ID" : 2, "name" : "Bench Press Declined", "weight" : "45", "reps" : 3, "rest" : 3 },
+      { "ID" : 3, "name" : "Contractor", "weight" : "27", "reps" : 2, "rest" : 5 },
+      { "ID" : 4, "name" : "Converging Press", "weight" : "45", "reps" : 4, "rest" : 7 }
     ]
   }
   localStorage.exs = JSON.stringify(exObj);
@@ -83,9 +70,59 @@ function getExercices(){
 
 /*Send to DB*/
 function addNewWorkout(exJSON){
+  alert(exJSON);
+  //send to db
+  location.href = 'workout.html';
+}
+
+function editEx(){
+  var ex = {
+    "IDWorkout" : localStorage.selectedWkID,
+    "name": $("#iexName").val(),
+    "weight" : $("#iexWeight").val(),
+    "rest" : $("#iexRest").val(),
+    "series" : $("#iexSeries").val(),
+    "annotations" : $("#iexAnn").val()
+  }
+
+
+  //send request to remove exercice localStorage.editExID of localStorage.selectedWkID
+
+  //send a request to add JSON.Stringfy(ex) in localStorage.selectedWkID
+
+  //alert(JSON.stringify(ex));
+
+  $("#iexName").val(" ");
+  $("#iexAnn").val(" ");
+  document.getElementById('modalEdit').style.display='none';
 
 }
 
-function addNewExercice(wkID){
+function addEx(){
+  var ex = {
+    "IDWorkout" : localStorage.selectedWkID,
+    "name": $("#aexName").val(),
+    "weight" : $("#aexWeight").val(),
+    "rest" : $("#aexRest").val(),
+    "series" : $("#aexSeries").val(),
+    "annotations" : $("#aexAnn").val()
+  }
+  //send a request to add JSON.Stringfy(ex) in localStorage.selectedWkID
 
+  //alert(JSON.stringify(ex));
+
+  $("#aexName").val(" ");
+  $("#aexAnn").val(" ");
+
+  document.getElementById('modalAdd').style.display='none';
+}
+
+function deleteEx(){
+  //send a request to remove exercice localStorage.editExID of localStorage.selectedWkID
+  document.getElementById('modalEdit').style.display='none';
+  location.href = 'edit.html';
+}
+
+function finishWorkout(){
+  alert(localStorage.finalWkResults);
 }
